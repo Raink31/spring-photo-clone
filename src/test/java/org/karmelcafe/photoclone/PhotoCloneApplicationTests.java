239c5c54b9809a3d4263cd36f4c5fd5c6c0a7ee1
Yohan Baes <yohan.baes97@gmail.com>
2024-01-20 15:26:27 +0100

    Initial commit

---
+++ src/test/java/org/karmelcafe/photoclone/PhotoCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.karmelcafe.photoclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
